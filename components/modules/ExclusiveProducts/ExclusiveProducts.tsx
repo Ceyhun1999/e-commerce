3dac873150ab5341c378f767c1cd005a07c1011d
Ceyhun1999 <rzayev-ceyhun-99@mail.ru>
2024-05-21 11:18:24 +0400

    test4

--- components/modules/ExclusiveProducts/ExclusiveProducts.tsx
+++ components/modules/ExclusiveProducts/ExclusiveProducts.tsx
@@ -1,7 +1,16 @@
+import { Suspense } from "react";
 import SectionProducts from "../SectionProducts/SectionProducts";
 
+function Loading() {
+    return <h2>🌀 Loading...</h2>;
+}
+
 const ExclusiveProducts = () => {
-    return <SectionProducts title="Exclusive Products" />;
+    return (
+        <Suspense fallback={<Loading />}>
+            <SectionProducts title="Exclusive Products" />
+        </Suspense>
+    );
 };
 
 export default ExclusiveProducts;
